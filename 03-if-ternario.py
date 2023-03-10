0d5b02d338833571795dab7d366383a70785c44c
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-10 11:19:39 -0300

    mensaje -if else

---
+++ 03-if-ternario.py
@@ -0,0 +1,14 @@
+edad = 19
+
+mensaje = "es mayor" if edad > 17 else "es menor"
+
+#el operador ternario es asignarle el valor a la variable con 
+#el valor que le aportamos en la izquieda O el valor de la derecha NECESARIAMENTE DEBE ESTAR ENTRE UN IF Y UN ELSE
+#if edad > 17:
+#    mensaje = "es mayor"
+
+#else:
+#    mensaje= "es menor"
+
+
+print(mensaje)
