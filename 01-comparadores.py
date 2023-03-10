70974ffc600333226db1c0e93f5701c954f90baf
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-09 23:14:40 -0300

    expressions explained

---
+++ 01-comparadores.py
@@ -0,0 +1,10 @@
+print(1 > 2)#expresion es una cadena de instrucciones que despues estas van a evaluar en una sola cosa
+#estas expresiones siempre entregan true o false
+print(1 < 2) #true
+print(1 <= 2)#true
+print(1 >= 2)#false
+print(2 == 2) #true
+print(2 == 3)#false
+print(2 == "2") #int no es igual a string 2 false
+print(2 != "2")
+print(2 != 2)#no es igual a ...
