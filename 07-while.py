78ac865520e056baf9eda186844a5bc6b703fa2f
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-10 14:59:13 -0300

    added a while exercise

---
+++ 07-while.py
@@ -0,0 +1,12 @@
+#numero = 1 
+#while numero < 100 :
+#    print(numero)
+#    numero *=
+
+
+
+while True:
+    comando = input("$ ")
+    print(comando)
+    if comando.lower ()== "salir":
+        break
