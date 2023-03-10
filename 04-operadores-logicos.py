f57f6c415086a7f8d871423d9a2cfbd13f856de5
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-10 11:44:01 -0300

    added a not and or exercise

---
+++ 04-operadores-logicos.py
@@ -0,0 +1,11 @@
+# and , or , not
+
+gas = False
+encendido = True
+edad = 18
+
+if not gas and ( encendido or edad >17) : #primero se evaluan los parentesis 
+    
+    print("puedes avanzar")
+
+#not NIEGA EL VALOR DEL STRING 
