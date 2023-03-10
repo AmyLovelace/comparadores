c9d5029fe9047cb74c84f689f56ac58aa5c04b1e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-10 14:39:09 -0300

    added a for else exercise

--- 06-for.py
+++ 06-for.py
@@ -1,7 +1,15 @@
 #FOR sirve para iterar una lista de elementos
 #range ejecuta el numeros de veces hasta el mismo valor()-1  
 
+buscar = 10
 for numero in range(5):
-    print(numero, numero * 'michi')
+    print(numero)
+    if numero == buscar:
+        print("encontrado", buscar)
+        break
+else:
+    print("no encontre el numnero buscado :(")     
 
-    
+
+
+
