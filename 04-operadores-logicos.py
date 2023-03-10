fae5d10572b5565bc12bce491d12cea50d96b409
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-10 13:55:12 -0300

    added a not or and exercise

--- 04-operadores-logicos.py
+++ 04-operadores-logicos.py
@@ -4,7 +4,9 @@ gas = False
 encendido = True
 edad = 18
 
-if not gas and ( encendido or edad >17) : #primero se evaluan los parentesis 
+if not gas or encendido or edad >17 : #primero se evaluan los parentesis 
+    #cuando hay dos AND pero el primer string es false no se evalua el resto de las expresiones de la derecha
+    #cuando es OR solo ace falta que el primer string sea verdadero .
     
     print("puedes avanzar")
 
